c4d51953215f73af087b124a7c512d570dd29f49
Omkar Ghadge <omkarghadge68@gmail.com>
2026-01-24 20:57:37 +0530

    Gallery project completed

--- Gallery Project/src/App.jsx
+++ Gallery Project/src/App.jsx
@@ -2,7 +2,7 @@ import Main from "./components/Main"
 
 const App = () => {
   return (
-    <div className="bg-zinc-200 h-screen w-full p-4" >
+    <div className="bg-zinc-200 min-h-screen w-full p-4" >
       <Main />
     </div>
   )
--- Gallery Project/src/components/Main.jsx
+++ Gallery Project/src/components/Main.jsx
@@ -1,34 +1,58 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Main = () => {
     const [userData, setUserData] = useState([]);
+    const [index, setIndex] = useState(1);
 
     async function getData() {
         const res = await axios.get(
-            "https://picsum.photos/v2/list?page=1&limit=100",
+            `https://picsum.photos/v2/list?page=${index}&limit=10`,
         );
-        console.log(res.data);
         setUserData(res.data);
     }
+
+    useEffect(
+        function () {
+            getData();
+        },
+        [index],
+    );
+
     return (
         <div>
-            <button
-                onClick={() => {
-                    getData();
-                }}
-                className="bg-zinc-700 text-white px-4 py-2 rounded-lg"
-            >
-                Get Data
-            </button>
-
-            <div>
-                <div className="grid grid-cols-4 gap-4 mt-4"></div>
+            <div className="flex flex-wrap gap-6 mt-4">
                 {userData.map((user) => (
-                    <div key={user.id}>
-                        <img src={user.download_url} alt={user.author} />
-                    </div>
+                    <a target="_blank" href={user.url} key={user.id} className="w-64">
+                        <img
+                            src={user.download_url}
+                            alt={user.author}
+                            className="w-full h-56 object-cover rounded-lg"
+                        />
+                        <p className="mt-2 text-md font-semibold text-center text-gray-800">
+                            {user.author}
+                        </p>
+                    </a>
                 ))}
+            </div>
+
+            <div className="flex justify-center mt-6">
+                <button
+                    onClick={() => {
+                        if (index > 1) setIndex(index - 1);
+                    }}
+                    className="bg-green-600 cursor-pointer active:scale-95 text-white px-4 py-2 rounded-lg mr-2"
+                >
+                    Prev
+                </button>
+                <button
+                    onClick={() => {
+                        setIndex(index + 1);
+                    }}
+                    className="bg-green-600 cursor-pointer active:scale-95 text-white px-4 py-2 rounded-lg"
+                >
+                    Next
+                </button>
             </div>
         </div>
     );
